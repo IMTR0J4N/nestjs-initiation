9214a66b0f7832de01035eded2503fca4fd3b4a8
IMTR0J4N <jjorgebeau.pro@gmail.com>
2025-03-04 22:52:39 +0100

    implement CRUD operations without logic for users in UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    /**
+     * TODO:
+        * PATCH /users/:id
+        * DELETE /users/:id
+     */
+
+    @Get() // 👈 GET /users or /users?role=value
+    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+        return [];
+    }
+
+    @Get(':id') // 👈 GET /users/:id
+    findOne(@Param('id') id: string) {
+        return { id };
+    }
+
+    @Post() // 👈 POST /users
+    create(@Body() user: {}) {
+        return user;
+    }
+
+    @Patch(':id') // 👈 PATCH /users/:id
+    update(@Param('id') id: string, @Body() userUpdate: {}) {
+        return { id, ...userUpdate };
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return { id };
+    }
+}
