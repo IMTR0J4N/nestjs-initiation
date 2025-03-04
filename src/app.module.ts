969ac78b83d68e3008319c50a956cc1610f40400
IMTR0J4N <jjorgebeau.pro@gmail.com>
2025-03-05 00:51:28 +0100

    feat: update throttling configuration in AppModule with multiple rate limits

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,11 @@ import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, EmployeesModule, ThrottlerModule.forRoot([{ ttl: 60000, limit: 10 }])],
+  imports: [UsersModule, DatabaseModule, EmployeesModule, ThrottlerModule.forRoot([{ name: "short", ttl: 1000, limit: 3 }, { name: "long", ttl: 60000, limit: 100 }])],
   controllers: [AppController],
   providers: [AppService, {
     provide: APP_GUARD,
     useClass: ThrottlerGuard,
   }],
 })
-export class AppModule {}
+export class AppModule { }
