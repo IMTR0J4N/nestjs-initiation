b997284e1a2a5bb61af1d2f277805cb49b1b0d10
IMTR0J4N <jjorgebeau.pro@gmail.com>
2025-03-04 23:58:11 +0100

    feat: enhance CreateUserDto with validation rules for name, email, and role

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,8 +1,18 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
+
+    @IsString()
+    @IsNotEmpty()
     name: string;
+
+    @IsEmail()
     email: string;
+
+    @IsEnum(['INTERN', 'ENGINEER', 'ADMIN'], {
+        message: 'Invalid role. Must be one of the following values: INTERN, ENGINEER, ADMIN'
+    })
     role: 'INTERN' | 'ENGINEER' | 'ADMIN';
 }
 
