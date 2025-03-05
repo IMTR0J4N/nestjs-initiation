f7a6365271848ef77c2a03a0856d76fc1f7d4a0c
IMTR0J4N <jjorgebeau.pro@gmail.com>
2025-03-05 01:41:54 +0100

    feat: add LoggerModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { DatabaseModule } from './database/database.module';
 import { EmployeesModule } from './employees/employees.module';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, EmployeesModule, ThrottlerModule.forRoot([{ name: "short", ttl: 1000, limit: 3 }, { name: "long", ttl: 60000, limit: 100 }])],
+  imports: [UsersModule, DatabaseModule, EmployeesModule, ThrottlerModule.forRoot([{ name: "short", ttl: 1000, limit: 3 }, { name: "long", ttl: 60000, limit: 100 }]), LoggerModule],
   controllers: [AppController],
   providers: [AppService, {
     provide: APP_GUARD,
