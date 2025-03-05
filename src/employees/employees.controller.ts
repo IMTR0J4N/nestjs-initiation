5344164dd1a1d7669c38f55202915d87bcd48507
IMTR0J4N <jjorgebeau.pro@gmail.com>
2025-03-05 01:41:59 +0100

    feat: enhance EmployeesController and EmployeesService with logging functionality

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseIntPipe } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma } from '@prisma/client';
+import { LoggerService } from 'src/logger/logger.service';
+
 
 @Controller('employees')
 export class EmployeesController {
-  constructor(private readonly employeesService: EmployeesService) {}
+  constructor(private readonly employeesService: EmployeesService) { }
+
+  private readonly Logger = new LoggerService(EmployeesController.name);
 
   @Post()
   create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
@@ -13,21 +17,45 @@ export class EmployeesController {
 
   @Get()
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return this.employeesService.findAll(role);
+    try {
+      this.Logger.log(`Requesting all employees ${role && `with role ${role}` || ``}`, EmployeesController.name);
+
+      return this.employeesService.findAll(role);
+    } catch (error) {
+      this.Logger.error(error, EmployeesController.name);
+    }
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.employeesService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    try {
+      this.Logger.log(`Requesting employee with id ${id}`, EmployeesController.name);
+
+      return this.employeesService.findOne(id);
+    } catch (error) {
+      this.Logger.error(error, EmployeesController.name);
+    }
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEmployeeDto: Prisma.EmployeeUpdateInput) {
-    return this.employeesService.update(+id, updateEmployeeDto);
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateEmployeeDto: Prisma.EmployeeUpdateInput) {
+    try {
+      this.Logger.log(`Requesting update of employee with id ${id}`, EmployeesController.name);
+
+      return this.employeesService.update(id, updateEmployeeDto);
+    } catch (error) {
+      this.Logger.error(error, EmployeesController.name);
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.employeesService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    try {
+      this.Logger.log(`Requesting deletion of employee with id ${id}`, EmployeesController.name);
+
+      return this.employeesService.remove(id);
+    } catch (error) {
+      this.Logger.error(error, EmployeesController.name);
+    }
   }
 }
--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,33 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
+import { LoggerService } from 'src/logger/logger.service';
 
 @Injectable()
 export class EmployeesService {
 
   constructor(private readonly databaseService: DatabaseService) { }
 
+  private readonly Logger = new LoggerService(EmployeesService.name);
+
   async create(createEmployeeDto: Prisma.EmployeeCreateInput) {
+
+    this.Logger.log(`Creating employee with email ${createEmployeeDto.email}`, EmployeesService.name);
+
     return this.databaseService.employee.create({ data: createEmployeeDto });
   }
 
   async findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
     if (role) {
+      this.Logger.log(`Fetching all employees ${role && `with role ${role}` || ``}`, EmployeesService.name);
+
       return this.databaseService.employee.findMany({ where: { role } });
     }
+
+    this.Logger.log(`Fetching all employees`, EmployeesService.name);
 
     return this.databaseService.employee.findMany();
   }
 
   async findOne(id: number) {
+    this.Logger.log(`Fetching employee with id ${id}`, EmployeesService.name);
+
     return this.databaseService.employee.findUnique({ where: { id } });
   }
 
   async update(id: number, updateEmployeeDto: Prisma.EmployeeUpdateInput) {
+    this.Logger.log(`Updating employee with id ${id}`, EmployeesService.name);
+
     return this.databaseService.employee.update({ where: { id }, data: updateEmployeeDto });
   }
 
   async remove(id: number) {
+    this.Logger.log(`Deleting employee with id ${id}`, EmployeesService.name);
+    
     return this.databaseService.employee.delete({ where: { id } });
   }
 }
