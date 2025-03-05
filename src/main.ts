002eb6cc231629861f8986357f7cc25708280040
IMTR0J4N <jjorgebeau.pro@gmail.com>
2025-03-05 01:42:08 +0100

    feat: integrate AllExceptionsFilter and custom LoggerService in main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggerService } from './logger/logger.service';
+import { AllExceptionsFilter } from './all-exceptions-filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { abortOnError: false, cors: true });
+  const app = await NestFactory.create(AppModule, { abortOnError: false, cors: true, bufferLogs: true });
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  app.useLogger(app.get(LoggerService));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
